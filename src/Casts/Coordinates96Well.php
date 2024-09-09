ab33ad0479803d1807495c35df11921352f99bd7
Benedikt Franke <benedikt.franke@mll.com>
2024-09-09 12:33:08 +0200

    Add `MLL\LaravelUtils\Casts\CoordinatesCast`

--- src/Casts/Coordinates96Well.php
+++ src/Casts/Coordinates96Well.php
@@ -2,37 +2,14 @@
 
 namespace MLL\LaravelUtils\Casts;
 
-use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
-use Illuminate\Database\Eloquent\Model;
-use MLL\Utils\Microplate\Coordinates;
+use MLL\Utils\Microplate\CoordinateSystem;
 use MLL\Utils\Microplate\CoordinateSystem96Well;
 
-/** @implements CastsAttributes<Coordinates<CoordinateSystem96Well>, Coordinates<CoordinateSystem96Well>> */
-class Coordinates96Well implements CastsAttributes
+/** @extends CoordinatesCast<CoordinateSystem96Well> */
+class Coordinates96Well extends CoordinatesCast
 {
-    /**
-     * @param  Model  $model
-     * @param  string  $key
-     * @param  array<array-key, mixed> $attributes
-     *
-     * @return Coordinates<CoordinateSystem96Well>
-     */
-    public function get($model, $key, $value, $attributes): Coordinates
+    protected function coordinateSystem(): CoordinateSystem
     {
-        assert(is_string($value));
-
-        return Coordinates::fromString($value, new CoordinateSystem96Well());
-    }
-
-    /**
-     * @param  Model  $model
-     * @param  string  $key
-     * @param  array<array-key, mixed>  $attributes
-     */
-    public function set($model, $key, $value, $attributes): string
-    {
-        assert($value instanceof Coordinates);
-
-        return $value->toString();
+        return new CoordinateSystem96Well();
     }
 }
