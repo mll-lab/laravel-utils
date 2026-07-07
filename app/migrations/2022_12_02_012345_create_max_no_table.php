7b8204027103d8a3b38460d51b5e5a7b5d710997
spawnia <spawnia@users.noreply.github.com>
2026-07-07 15:04:30 +0000

    Apply php-cs-fixer changes

--- app/migrations/2022_12_02_012345_create_max_no_table.php
+++ app/migrations/2022_12_02_012345_create_max_no_table.php
@@ -3,7 +3,7 @@
 use App\Database\MaxNo;
 use Illuminate\Database\Migrations\Migration;
 
-return new class() extends Migration {
+return new class extends Migration {
     public function up(): void
     {
         MaxNo::createTable();
